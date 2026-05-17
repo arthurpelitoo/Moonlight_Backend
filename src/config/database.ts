c33bc54a2e4cb9a1455615b5a7e56a184d44ba38
arthurpelitoo <arthurantoniocontact@gmail.com>
2026-05-17 13:37:01 -0300

    antes de implementar gitflow, mudei o arquivo .env de exemplo e database

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || '',
-        database: 'moonlight',
+        database: process.env.DB_DATABASE_NAME || 'moonlight',
     waitForConnections: true,
   connectionLimit: 10
 });
