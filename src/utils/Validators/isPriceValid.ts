77b190aac493b07400629c7a387a8b045b3e6292
arthurpelitoo <arthurantoniocontact@gmail.com>
2026-05-19 03:24:06 -0300

    fix: importações corretas, seguindo o padrão atual de codigo.

--- src/utils/Validators/isPriceValid.ts
+++ src/utils/Validators/isPriceValid.ts
@@ -4,6 +4,10 @@ export function isPriceValid(price: unknown): boolean {
   const num = Number(price);
 
   return (
+<<<<<<< HEAD
     Number.isFinite(num) && num >= 0 && Number.isInteger(Math.round(num * 100)) // máximo 2 casas decimais
+=======
+    Number.isFinite(num) && num >= 0 && Number.isInteger(num * 100) // máximo 2 casas decimais
+>>>>>>> 1145c08 (fix: importações corretas, seguindo o padrão atual de codigo.)
   );
 }
