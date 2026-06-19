0b7d95f30bd0891b6b18ecac7919984b8ed89677
arthurpelitoo <arthurantoniocontact@gmail.com>
2026-06-19 20:21:49 -0300

    feat(config): configura husky com validação de commit e pre-push

--- src/servers/server.ts
+++ src/servers/server.ts
@@ -30,10 +30,10 @@ app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
-// if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== 'test') {
 app.listen(PORT, () => {
     console.log(`Servidor na porta ${PORT}`);
 });
-// }
+}
 
 export default app;
