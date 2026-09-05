adb768839bcd0f47816c045d22520fcc0930954b
arthurpelitoo <arthurantoniocontact@gmail.com>
2026-09-05 02:03:27 -0300

    fix(error): corrigido bug no checkoutController handleWebhook que causaria erros constantes

--- src/services/checkout.Service.ts
+++ src/services/checkout.Service.ts
@@ -18,7 +18,7 @@ export class CheckoutService{
                 const canBuy = await this.orderService.canPurchaseGame(dto.user.id_user, item.id_game);
                 if (!canBuy) throw new AppError(`Você já possui o jogo "${item.title}" na sua biblioteca.`, 400, "USER_ALREADY_HAVE_GAME");
             }
-            
+
             const client = this.createMercadoPagoClient();
             const preferenceClient = new Preference(client);
 
@@ -28,7 +28,7 @@ export class CheckoutService{
             const id_order = await this.createOrder(dto, preference.id!, external_reference);
 
             await this.createPurchasedItems({id_order, items: dto.items} as PurchasedItemsDTO);
-            
+
             return preference.id;
         }
 
